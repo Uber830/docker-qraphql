246151d3f29fd3392befa2280b42a9df734b37ce
Uber830 <uber.cardoso@uniminuto.edu.co>
2026-03-24 01:50:12 -0500

    feat: Update main.ts to include application name from environment variable and bind to 0.0.0.0

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const NameApp = process.env.NAME_APP || '';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -12,8 +13,8 @@ async function bootstrap() {
     })
   );
 
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
   
-  console.log('Aplicación corriendo en puerto: ', 3000 );
+  console.log(`Aplicación ${NameApp} corriendo en puerto: ${3000}`);
 }
 bootstrap();
